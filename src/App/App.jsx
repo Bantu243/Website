30cd537f75fdd929a5949395dc64e4c14112fee5
Bantu243 <dsalogale@wbsedu.de>
2025-04-03 10:29:51 +0200

    App-Pages

---
+++ src/App/App.jsx
@@ -0,0 +1,36 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from './pages/Contact';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function App() {
+return (
+
+        <Router>
+
+    <div className='App'>
+        <Header />
+
+<main className='p-8'>
+        
+        <Routes>
+            <Route path='/' element = {<Home />} />
+            <Route path='/about' element = {<About />} />
+            <Route path='/contact' element = {<Contact />} />
+        </Routes>
+
+</main>
+        <Footer />
+    </div>
+
+</Router>
+);
+}
+
+
+export default App;
