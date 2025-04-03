7024b0a97e1f53dbd77fa82319ab1d4bd5c85627
Bantu243 <dsalogale@wbsedu.de>
2025-04-03 07:58:25 +0200

    New

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function App() {
+return (
+    <div className='App'>
+        <Header />
+
+    <main className='p-8'>
+        <h1 classname= "text-4xl font-bold">Willkommen auf meinem Portfolio</h1>
+        <p>Hier ist ein Überblick über meine Projekte und Fähigkeiten.</p>
+
+</main>
+<Footer />
+</div>
+
+);
+}
+
+
+export default App;
