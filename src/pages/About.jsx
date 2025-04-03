4584efd36db384b9cedb5327b85297dc425a39a6
Bantu243 <dsalogale@wbsedu.de>
2025-04-03 13:43:26 +0200

    Fresh

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { motion } from "framer-motion";
+
 
 const About = () => {
 
-    return <h1>Über mich</h1>;
+    return (
+        <motion.div
+            initial= {{ x: '100vw'}}
+            animate={{x: 0 }}
+            exit={{ x: '-100vw' }}
+            transition={{ type: 'spring', stiffness: 100 }}
+        >
+            <h1 className="text-4xl font-bold">Über mich</h1>
+            <p>Hier ist ein kurze Beschreibung über meine Arbeit und meine Fähigkeiten.</p>
+
+        </motion.div>
+
+    );
 };
 
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { motion } from "framer-motion";
 
-const Home = () => {
+const Home = () => 
+    {
 
-    return <h1>Startseite</h1>;
-};
+return (
+
+    <motion.div
+        initial= {{ opacity: 0 }}
+        animate= {{opacity: 1 }}
+        exit= {{ opacity: 0}}
+        transition={{ duration: 1 }}
+    >
+  <h1 className="text-4xl font-bold">Willkommen auf der Home-Seite</h1>
+  <p>Hier kannst du mehr über mich erfahren.</p>
+  
+  </motion.div>
+  
+    );
+   };
 
 export default Home;
